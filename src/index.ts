a14fc5f54db3bfe567818a3b4d3ced2de5cb619a
ROHIT <rohityadav3461@gmail.com>
2025-04-05 15:38:38 +0530

    corrected the signup and added the sigin endpoint return jwt

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import express from "express";
 import "dotenv/config";
 import { User } from "./db";
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
 
 const app = express();
 const port = 3000;
+const JWT_SECRET = process.env.JWT_SECRET_USER;
 
 app.use(express.json());
 
@@ -12,9 +14,9 @@ app.post("/api/v1/signup", async (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
-  const query = await User.find({
+  const query = await User.findOne({
     username,
-    password
+    password,
   });
 
   if (query) {
@@ -22,13 +24,36 @@ app.post("/api/v1/signup", async (req, res) => {
       data: "You already signup",
     });
   } else {
+    await User.create({
+      username,
+      password,
+    });
     res.status(200).json({
       data: "success",
     });
   }
 });
 
-app.post("/api/v1/signin", (req, res) => {});
+app.post("/api/v1/signin", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const query = await User.find({
+    username,
+    password,
+  });
+
+  if (!query) {
+    res.status(403).json({
+      data: "Invalid Credentials",
+    });
+  } else {
+    const token = jwt.sign({ username: username }, JWT_SECRET!);
+    res.status(200).json({
+      data: token,
+    });
+  }
+});
 
 app.post("/api/v1/content", (req, res) => {});
 
